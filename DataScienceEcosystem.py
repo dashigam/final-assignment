946807cc59630330f75b1bb77d9b1c17b2a6bfe0
dashigam <90917890+dashigam@users.noreply.github.com>
2023-05-07 17:21:29 +0630

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Econsystem 
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized.Some of the popular languages that Data Scienctists use are: 
+# 
+# - Python
+# - R
+# - SQL
+# - Java
+# - Julia
+
+# **Objectives**
+# - Provide an introduction to Data Science
+# - List popular languages for Data Science
+# - Descrbe commonly used libraries for Data Science
+# - Explain basic concepts and techniques in Data Science
+
+# ## Author
+# D Aung
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# - Numpy
+# - Pandas
+# - Matplotlib
+# - Seaborn
+# - Scikit-learn
+# - TensorFlow
+# - PyTorch
+# 
+
+# |Data Science Tools (Development Environment Open Source Tools)|
+# |:--------------------------------------------------------------|
+# | Jupyter Notebook                                             |
+# | RStudio                                                      |
+# | Spyder                                                       |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# You can use Python as a calculator to perform basic arithmetic operations such as addition (+), subtraction (-), multiplication (*), division (/), and exponentiation (**). 
+# 
+# For example:
+# - 5+3
+# - 10-2
+# - 6*3
+# - 16/2
+# - 2**3
+# 
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers.
+#Then execute the cell to ensure the expression returns the expected output of 17.
+(3*4)+5
+
+
+# In[4]:
+
+
+#This will convert 200 minutes to hours by diving by 60.
+200/60
+
